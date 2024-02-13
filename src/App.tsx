b787f094f137ff5f119195d7d5665a37281030fd
james <james.barbosa@gmail.com>
2024-02-13 19:44:26 +0800

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './assets/components/Header'
 
 function App() {
 
-  return 
-    <>
-    </>
-  
+  return <>
+    <Header />
+  </>
+
 }
 
 export default App
---
+++ src/assets/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return <header>
+        <h1>Quiz App</h1>
+    </header>
+}
